ca246e5aa3ca816598f6ca82c0c54eb3f0e44b2b
sivane guedj <sivane@g.jct.ac.il>
2024-02-13 10:22:08 +0200

    profile score

--- JS/profile.js
+++ JS/profile.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     // Retrieve the username from local storage
-    console.log("hi");
     var username = JSON.parse(localStorage.getItem('user'));
-    console.log(username);
 
     // Check if the user is logged in
-    if (username) {
-        // Display the username in the profile page
-        var userDetails = document.querySelector('.user-details');
-        userDetails.innerHTML = '<p><strong>Username:</strong> ' + username + '</p>';
-    } else {
+    if (!username) {
         // Redirect to the login page if the user is not logged in
         window.location.href = "../HTML/login.html";
     }
+
+    // Display the username in the profile page
+    var userDetails = document.querySelector('.user-details');
+    userDetails.innerHTML = '<p><strong>Username:</strong> ' + username + '</p>';
 
 
     // Parse users data from local storage
@@ -22,15 +21,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var usersWithGame2Score = users.filter(user => user.scoreGame2);
 
     // Sort users based on their scores for "Game 2" in descending order
-    usersWithGame2Score.sort(function(a, b) {
-        var scoreA = parseInt(a.scoreGame2.split(' ')[0]);
-        var scoreB = parseInt(b.scoreGame2.split(' ')[0]);
-        return scoreB - scoreA;
-    });
+usersWithGame2Score.sort(function(a, b) {
+    var scoreA = 0;
+    var scoreB = 0;
+    if (typeof a.scoreGame2 === 'string') {
+        scoreA = parseInt(a.scoreGame2.split(' ')[0]);
+    }
+    if (typeof b.scoreGame2 === 'string') {
+        scoreB = parseInt(b.scoreGame2.split(' ')[0]);
+    }
+    return scoreB - scoreA;
+});
 
     // Display the top three users with the highest scores for "Game 2"
     var recordsTable = document.querySelector('.records table');
-    for (var i = 0; i < Math.min(3, usersWithGame2Score.length); i++) {
+    recordsTable.innerHTML = '';
+    for (var i = 0; i < usersWithGame2Score.length; i++) {
         var user = usersWithGame2Score[i];
         var row = document.createElement('tr');
         row.innerHTML = `
